a10e55cad2636549654893060dff684cfe00f7be
Luis Yago Fernández <luisyagofdez@gmail.com>
2024-02-08 14:19:11 +0100

    DOCS(readme): update and extend readme; improve texts

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="about">
     <h1>What is this about?</h1>
-    <h4>This is a collection of functionalities for a SPA and a personal playground for new technologies from the
+    <h4>This is a collection of utilities for a SPA and a personal playground for new technologies from the
       Vue
       ecosystem.</h4>
-    <h4>The functionalities displayed include an infinite scroll, an inactivity timeout, a click-outside-an-HTML-node
+    <h4>The utilities displayed include an infinite scroll, an inactivity timeout, a click-outside-an-HTML-node
       directive, etc.</h4>
     <h4>The technologies I'm testing include Playwright, Vitest, Pinia and more</h4>
   </div>
